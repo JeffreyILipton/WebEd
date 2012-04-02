811c378146636d1938651ec9d118b590b318ba19
Jeffrey Lipton <jeffreyilipton@gmail.com>
2012-04-01 23:01:00 -0400

    fixing up css

--- mysite/app.js
+++ mysite/app.js
@@ -31,42 +31,65 @@ app.configure('production', function(){
 
 portinterface = new PortfolioInterface("localhost", 27017);	
 
-var navbar = [{link:"http://www.google.com","text":"Home"},
-	          {link:"#","text":"About"},
-	          {link:"#","text":"Blog"},
-	          {link:"#","text":"Contact"}];
+var navbar = [{link:"/","text":"Home"},
+	          {link:"/about","text":"About"},
+	          {link:"/media","text":"Media"},
+	          {link:"/portfolio","text":"portfolio"},
+	          {link:"/contact","text":"Contact"}];
 
 var sides =[{link:"http://www.google.com","txt":"google"},
             {link:"http://www.bing.com","txt":"bing"},
             {link:"http://www.yahoo.com","txt":"yahoo"}];
 
 /*var grids = [{image:"http://d2o0t5hpnwv4c1.cloudfront.net/373_html5/final/images/flower.png",link:"http://www.google.com","hdr":"woot","txt":"Awww yeah"},
-			 {image:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.jpg", link:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.html","hdr":"Hotel Innovation","txt":"Presented at the Cornell NYC innovation network"},
-			 {image:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.jpg", link:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.html","hdr":"O snap","txt":"Im fucking awesome"},
-			 {image:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.jpg", link:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.html","hdr":"O snap","txt":"Im fucking awesome"},
-			 {image:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.jpg", link:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.html","hdr":"O snap","txt":"Im fucking awesome"},
-			 {image:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.jpg", link:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.html","hdr":"O snap","txt":"Im fucking awesome"},
-			 {image:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.jpg", link:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.html","hdr":"O snap","txt":"Im fucking awesome"}];
+			 {image:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.jpg", link:"http://www.news.cornell.edu/stories/Dec11/NYCInnovation.html","hdr":"Hotel Innovation","txt":"Presented at the Cornell NYC innovation network"},];
 */
 
 // Routes
 app.get('/', function(req,res){
     portinterface.getMedia(function(error, grids){
 		res.render('index.jade',{locals:{
-			title:"TESTING",
+			title:"JeffreyILipton.com",
 			navItems: navbar,
 			sideItems:sides,
+			intro:{hdr:"Welcome to JeffreyILipton.com",
+			       txt:"This is the personal website for Jeffrey Lipton"},
 			gridItems:grids
+			}});
+		});
+});
+
+app.get('/portfolio', function(req,res){
+    portinterface.getMedia(function(error, grids){
+		res.render('boxdisplays.jade',{locals:{
+			title:"Portfolio",
+			navItems: navbar,
+			sideItems:sides,
+			sectionItems:[{id:"talks", hdr:"Talks and Outreach", gridItems:grids},
+						  {id:"projects", hdr:"Projects", gridItems:grids}]			
+			}});
+		});
+});
+
+app.get('/media', function(req,res){
+    portinterface.getMedia(function(error, grids){
+		res.render('boxdisplays.jade',{locals:{
+			title:"Media Coverage",
+			navItems: navbar,
+			sideItems:sides,
+			sectionItems:[{id:"print", hdr:"Print Articles", gridItems:grids},
+						  {id:"videos", hdr:"Videos", gridItems:grids},
+						  {id:"webarticles",hdr:"Online Articles", gridItems:grids}]			
 			}});
 		});
 });
 
 app.get('/media/new', function(req, res) {
     res.render('media_new.jade', { locals: {
-        title: 'New Media',
+        title: 'Set New Media',
         navItems: navbar,
 		sideItems:sides,
-        blankimage:"http://d2o0t5hpnwv4c1.cloudfront.net/373_html5/final/images/flower.png"
+        blankimage:""
     }
     });
 });
