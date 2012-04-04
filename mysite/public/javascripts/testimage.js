86f41c8632537c819526a8424b88ad62ea4d6e2d
Jeff <jeff@ServeBob.(none)>
2012-04-04 04:27:10 -0400

    fixed image issues with early load

--- mysite/public/javascripts/testimage.js
+++ mysite/public/javascripts/testimage.js
@@ -9,21 +9,26 @@ $(document).ready(function(){
 	    
 	    var w = bw/iw;
 	    var h = bh/ih;
-	    
+	    var m=1;
 	    if((iw==0)&&(ih==0)){
 			console.log('w,h '+w+','+h);
+			w=bw;
+			h=bh;
 		}else{
-			var m = Math.min(w,h);
-			w = m*iw;
+     		m = Math.min(w,h);
+     		w = m*iw;
 			h = m*ih;
-			$('img',this).css("width",w+'px');
-			$('img',this).css("height",h+'px');
-			w = (bw-w)/2.0;
-			h = (bh-h)/2.0;
-			$('img',this).css("margin-left",w+'px');
-			$('img',this).css("margin-top",h+'px');
-			
 		}
+		
+
+		$('img',this).css("width",w+'px');
+		$('img',this).css("height",h+'px');
+		w = (bw-w)/2.0;
+		h = (bh-h)/2.0;
+		$('img',this).css("margin-left",w+'px');
+		$('img',this).css("margin-top",h+'px');
+			
+		
 	    
     };
     $('.boxgrid').each(sizer)
