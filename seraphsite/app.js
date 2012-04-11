f8f64104ea596e9b329f6c08a3ee04dd5beb1740
Jeff <jeff@ServeBob.(none)>
2012-04-11 15:08:44 -0400

    Added the app selectors to the main page of seraph site. Its now sexy.

--- seraphsite/app.js
+++ seraphsite/app.js
@@ -45,13 +45,30 @@ var sides =[{link:"http://www.fabathome.org","txt":"Top Picks"},
             {link:"http://www.fabathome.org","txt":"Arts/Crafts"},
             {link:"http://www.fabathome.org","txt":"Tech/Gizmos"}];
 
+var apps=[{hdr:'Logo2Fab',txt:'Program a Shape using Logo',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Cool Cookie',txt:'Create your own cookie',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Scrapbook Cutter',txt:'Create Letters and Cutouts',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Birdhouse',txt:'Build a home for birds',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Physics Ramp',txt:'Learn about gravity',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Jewlery Box',txt:'Custom for all your gems',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Chocolate Design',txt:'Draw any shape in sweetnes',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'RaceCar',txt:'Make your own RC car',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Custom Blocks',txt:'Make your own LEGO blocks',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Battery Cover',txt:'Lost it? Make it!',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'House Hooks',txt:'hand it up right away',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Iphone Stand',txt:'stop droping your phone',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Wacky Wristband',txt:'draw the shape and wear the band',src:"/images/SeraphIcon-5-blue.png",link:"#"},
+{hdr:'Chess Set',txt:'Customize the set for you',src:"/images/SeraphIcon-5-blue.png",link:"#"}
+]
+
 // Routes
 app.get('/', function(req,res){
-	res.render('index.jade',{locals:{
-			  title:"SeraphStore",
-              sideItems:sides
-			  }
-	});
+    res.render('index.jade',{locals:{
+              title:"SeraphStore",
+              sideItems:sides,
+              appdisplays:apps
+              }
+    });
 });
 
 
